0180384b3648f0ca5b7855d0e5092e1df236b8f0
Miguel Flores <miguelacio@beardboy.mx>
2017-08-11 17:20:38 -0600

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response));
+    }
+    render() {
+        return (
+            <View>
+                <Text> Albummmmmm</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;           
